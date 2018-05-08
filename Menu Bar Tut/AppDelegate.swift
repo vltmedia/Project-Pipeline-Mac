e16023ea24442ea91b7a06b6ea85fbed08d89b9b
Justin Jaro <info@VLTMedia.com>
2018-05-07 21:22:05 -0400

    Initial Commit

---
+++ Menu Bar Tut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Menu Bar Tut
+//
+//  Created by Justin Jaro on 5/7/18.
+//  Copyright © 2018 Justin Jaro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Menu Bar Tut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Menu Bar Tut
+//
+//  Created by Justin Jaro on 5/7/18.
+//  Copyright © 2018 Justin Jaro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
